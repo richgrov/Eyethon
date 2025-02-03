19771222ad45dfee125db1c539c6e5b07d6f21b0
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-03 08:20:21 -0700

    Delete old C code

--- src/array.h
+++
@@ -1,6 +0,0 @@
-#ifndef FOX_ARRAY_H_
-#define FOX_ARRAY_H_
-
-#define FOX_ARRLEN(x) (sizeof(x) / sizeof(0 [x]))
-
-#endif // !FOX_ARRAY_H_
--- src/main_test.c
+++
@@ -1,8 +0,0 @@
-#include "preprocessor/macro_dict_test.h"
-
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-   test_macro_dict();
-   puts("OK");
-}
