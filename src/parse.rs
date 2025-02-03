fcbdd21f42eb7e8ab0809f5f0a0c91e23db9795e
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-03 11:47:28 -0700

    Begin parsing annotations

--- src/parse.rs
+++ src/parse.rs
@@ -10,10 +10,32 @@ pub struct Statement {
 }
 
 #[derive(Debug)]
-pub enum StatementType {}
+pub enum StatementType {
+    Annotation {
+        annotation: Box<Expression>,
+        target: Box<Statement>,
+    },
+}
+
+#[derive(Debug)]
+pub struct Expression {
+    pub ty: ExpressionType,
+    pub line: usize,
+    pub column: usize,
+}
+
+#[derive(Debug)]
+pub enum ExpressionType {
+    Identifier(String),
+    FunctionCall(FunctionCallExpression),
+}
+
+#[derive(Debug)]
+pub struct FunctionCallExpression {}
 
 pub enum ParseError {
     UnexpectedToken(Token),
+    UnexpectedEof,
 }
 
 impl fmt::Display for ParseError {
@@ -21,6 +43,9 @@ impl fmt::Display for ParseError {
         match self {
             ParseError::UnexpectedToken(tok) => {
                 write!(f, "{}:{}: unexpected token {:?}", tok.line, tok.column, tok)
+            }
+            ParseError::UnexpectedEof => {
+                write!(f, "{}:{}: unexpected end of file", 0, 0)
             }
         }
     }
@@ -33,21 +58,67 @@ struct Parser {
 
 impl Parser {
     fn new(tokens: Vec<Token>) -> Self {
+        let without_comment = tokens
+            .into_iter()
+            .filter(|tok| {
+                std::mem::discriminant(&tok.ty)
+                    != std::mem::discriminant(&TokenType::Comment {
+                        text: "".to_owned(),
+                    })
+            })
+            .collect();
+
         Self {
-            tokens,
+            tokens: without_comment,
             read_index: 0,
         }
     }
 
     fn next(&mut self) -> Option<Result<Statement, ParseError>> {
-        Some(loop {
-            let token = self.next_token()?;
+        if let None = self.peek_tok() {
+            return None;
+        }
 
-            match token.ty {
-                TokenType::Comment { .. } => {}
-                _ => break Err(ParseError::UnexpectedToken(token.clone())),
-            }
+        Some(self.annotation())
+    }
+
+    fn annotation(&mut self) -> Result<Statement, ParseError> {
+        let first_tok = match self.next_token() {
+            Some(tok) => tok.clone(),
+            None => return Err(ParseError::UnexpectedEof),
+        };
+
+        let TokenType::At = first_tok.ty else {
+            return Err(ParseError::UnexpectedToken(first_tok));
+        };
+
+        let annotation = self.expression()?;
+        let target = self.annotation()?;
+
+        Ok(Statement {
+            ty: StatementType::Annotation {
+                annotation: Box::new(annotation),
+                target: Box::new(target),
+            },
+            line: first_tok.line,
+            column: first_tok.column,
         })
+    }
+
+    fn expression(&mut self) -> Result<Expression, ParseError> {
+        let token = match self.next_token() {
+            Some(token) => token.clone(),
+            None => return Err(ParseError::UnexpectedEof),
+        };
+
+        match token.ty {
+            TokenType::Identifier(name) => Ok(Expression {
+                ty: ExpressionType::Identifier(name),
+                line: token.line,
+                column: token.column,
+            }),
+            _ => Err(ParseError::UnexpectedToken(token)),
+        }
     }
 
     fn peek_tok(&self) -> Option<&Token> {
