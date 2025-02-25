fd2657bcf75235b40ea00db34cc4224e4ef66439
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-25 04:28:26 -0700

    Add GDScript tests from godot repository

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,82 @@ mod tests {
     use crate::vm::*;
     use crate::*;
 
+    struct Test {
+        name: String,
+        source: String,
+        output: Option<String>,
+    }
+
+    fn get_tests(category: &str) -> impl Iterator<Item = Test> {
+        std::fs::read_dir("tests/".to_owned() + category)
+            .unwrap()
+            .filter_map(|file| file.ok().map(|file| file.path()))
+            .filter(|path| match path.extension() {
+                Some(ext) if ext == "gd" => true,
+                _ => false,
+            })
+            .map(|path| {
+                let file_name = path
+                    .file_name()
+                    .unwrap()
+                    .to_os_string()
+                    .into_string()
+                    .unwrap();
+
+                let output = if file_name.ends_with(".notest.gd") {
+                    None
+                } else {
+                    Some(
+                        std::fs::read_to_string(path.with_extension("out"))
+                            .expect(&format!("failed to read test output for {:?}", path)),
+                    )
+                };
+
+                Test {
+                    name: path.clone().into_os_string().into_string().unwrap(),
+                    source: std::fs::read_to_string(&path).expect("failed to read test source"),
+                    output,
+                }
+            })
+    }
+
+    fn run_test(test: &Test) -> bool {
+        let tokens = match tokenize::tokenize(&test.source) {
+            Ok(t) => t,
+            Err(e) => {
+                eprintln!("tokenization for {} failed: {}", test.name, e);
+                return false;
+            }
+        };
+
+        let _ = match parse::parse(tokens) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("parse {} failed: {}", test.name, e);
+                return false;
+            }
+        };
+
+        true
+    }
+
+    #[test]
+    fn parser_features() {
+        let mut failed = 0;
+        let mut total = 0;
+
+        for test in get_tests("parser/features") {
+            if !run_test(&test) {
+                failed += 1;
+            }
+            total += 1;
+        }
+
+        if failed > 0 || total == 0 {
+            panic!("{}/{} cases failed", failed, total);
+        }
+    }
+
     #[test]
     fn main() {
         let file = "<internal>";
