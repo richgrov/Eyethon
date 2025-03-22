8b38fa52c561fe835e65b5545ce4be7559e5ee21
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-03-21 19:58:32 -0600

    Remove the tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+use core::fmt;
 use std::collections::HashMap;
 use std::rc::Rc;
 
-use interpret::Interpreter;
+use inference::Inference;
+use interpret::{Interpreter, Value};
 
 mod compile;
 mod inference;
@@ -24,27 +26,28 @@ impl Vm {
         }
     }
 
-    pub fn run(&mut self, source: &str, fallback_class_name: String) -> Result<(), Error> {
+    pub fn register_class(&mut self, file_name: String, source: &str) -> Result<(), Error> {
         let tokens = tokenize::tokenize(source).map_err(|e| Error::SyntaxError(e))?;
         let ast = parse::parse(tokens).map_err(|e| Error::ParseError(e))?;
-        let class = compile::compile(
-            ast,
-            HashMap::new(),
-            fallback_class_name.clone(),
-            self.inference.clone(),
-        )
-        .map_err(|e| Error::CompileError(e))?;
+        let class = compile::compile(ast, HashMap::new(), self.inference.clone())
+            .map_err(|e| Error::CompileError(e))?;
 
-        self.interpreter.register_class(class).unwrap();
+        for (i, instr) in class.bytecode.iter().enumerate() {
+            println!("{:04} {}", i, instr);
+        }
 
-        self.interpreter
-            .new_instance(&fallback_class_name)
-            .map_err(|e| Error::RuntimeError(e))?;
-
+        self.interpreter.register_file(file_name, class);
         Ok(())
+    }
+
+    pub fn exec(&mut self, file_name: &str, id: usize) -> Result<Option<Value>, Error> {
+        self.interpreter
+            .exec(file_name, id)
+            .map_err(|e| Error::RuntimeError(e))
     }
 }
 
+#[derive(Debug)]
 pub enum Error {
     SyntaxError(tokenize::TokenizerError),
     ParseError(parse::ParseError),
@@ -52,219 +55,44 @@ pub enum Error {
     RuntimeError(interpret::RuntimeError),
 }
 
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::SyntaxError(ref e) => e.fmt(f),
+            Error::ParseError(ref e) => e.fmt(f),
+            Error::CompileError(ref e) => e.fmt(f),
+            Error::RuntimeError(ref e) => e.fmt(f),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::cell::RefCell;
-    use std::collections::HashMap;
-    use std::rc::Rc;
-
-    use interpret::Value;
-
     use crate::*;
 
-    struct Test {
-        name: String,
-        source: String,
-        output: Option<String>,
-    }
+    fn run(source: &str, expected_output: &str) {
+        let mut vm = Vm::new();
 
-    fn get_tests(category: &str) -> impl Iterator<Item = Test> {
-        std::fs::read_dir("tests/".to_owned() + category)
-            .unwrap()
-            .filter_map(|file| file.ok().map(|file| file.path()))
-            .filter(|path| match path.extension() {
-                Some(ext) if ext == "gd" => true,
-                _ => false,
-            })
-            .map(|path| {
-                let file_name = path
-                    .file_name()
-                    .unwrap()
-                    .to_os_string()
-                    .into_string()
-                    .unwrap();
-
-                let output = if file_name.ends_with(".notest.gd") {
-                    None
-                } else {
-                    Some(
-                        std::fs::read_to_string(path.with_extension("out"))
-                            .expect(&format!("failed to read test output for {:?}", path)),
-                    )
-                };
-
-                Test {
-                    name: path.clone().into_os_string().into_string().unwrap(),
-                    source: std::fs::read_to_string(&path).expect("failed to read test source"),
-                    output,
-                }
-            })
-    }
-
-    fn run_test(test: &Test, class: compile::ClassBytecode) -> bool {
-        println!("{} bytecode:", test.name);
-        for (i, instr) in class.bytecode.iter().enumerate() {
-            println!("{:04} {}", i, instr);
-        }
-        println!("{:?}", class.functions);
-
-        let class_name = class.name.clone();
-
-        let mut interpreter = interpret::Interpreter::new();
-
-        let output = Rc::new(RefCell::new("GDTEST_OK\n".to_owned()));
-        let out = output.clone();
-
-        interpreter.set_global(
-            "is_equal_approx",
-            Value::NativeFunction(Rc::new(move |args| {
-                if args.len() != 2 {
-                    return Value::Bool(false);
-                }
-
-                let a = match &args[0] {
-                    Value::Float(f) => *f,
-                    Value::Integer(i) => *i as f64,
-                    _ => return Value::Bool(false),
-                };
-
-                let b = match &args[1] {
-                    Value::Float(f) => *f,
-                    Value::Integer(i) => *i as f64,
-                    _ => return Value::Bool(false),
-                };
-
-                Value::Bool(a == b)
-            })),
-        );
-
-        interpreter.set_global(
-            "print",
-            Value::NativeFunction(Rc::new(move |args| {
-                let mut output = out.borrow_mut();
-
-                for arg in args {
-                    output.push_str(&format!("{}", arg));
-                }
-                output.push('\n');
-
-                Value::Null
-            })),
-        );
-
-        if let Err(e) = interpreter.register_class(class) {
-            eprintln!("register class failed: {}", e);
-            return false;
-        }
-
-        let object = match interpreter.new_instance(&class_name) {
-            Ok(o) => o,
-            Err(e) => {
-                eprintln!("instantiation of {} failed: {}", test.name, e);
-                return false;
-            }
+        if let Err(e) = vm.register_class("test".to_owned(), source) {
+            panic!("{}", e);
         };
 
-        if let Some(expected) = &test.output {
-            match interpreter.call_method(object, 0) {
-                Ok(Some(obj)) => {
-                    eprintln!("{} returned a value: {}", test.name, obj);
-                    return false;
-                }
-                Ok(None) => {}
-                Err(e) => {
-                    eprintln!("call {} failed: {}", test.name, e);
-                    return false;
-                }
-            }
-
-            let actual = output.borrow();
-            if actual.ne(expected) {
-                eprintln!("-- expected output for {} --", test.name);
-                eprintln!("{}", expected);
-                eprintln!("-- actual output --");
-                eprintln!("{}", actual);
-                eprintln!("-- end --");
-                return false;
-            }
+        if let Err(e) = vm.exec("test", 0) {
+            panic!("{}", e);
         }
-
-        true
     }
 
-    fn run_tests(tests: impl Iterator<Item = Test>) {
-        let mut total_tokenized = 0;
-        let mut failed_tokenized = 0;
-        let mut total_parsed = 0;
-        let mut failed_parsed = 0;
-        let mut total_compiled = 0;
-        let mut failed_compiled = 0;
-        let mut total_runs = 0;
-        let mut failed_runs = 0;
+    #[test]
+    fn test() {
+        run(
+            "
+when the program starts,
+    greet the user
 
-        let classes = tests
-            .filter_map(|test| {
-                total_tokenized += 1;
-                match tokenize::tokenize(&test.source) {
-                    Ok(tokens) => Some((test, tokens)),
-                    Err(error) => {
-                        eprintln!("failed to tokenize {}: {}", test.name, error);
-                        failed_tokenized += 1;
-                        None
-                    }
-                }
-            })
-            .filter_map(|(test, tokens)| {
-                total_parsed += 1;
-                match parse::parse(tokens) {
-                    Ok(statements) => Some((test, statements)),
-                    Err(error) => {
-                        eprintln!("failed to parse {}: {}", test.name, error);
-                        failed_parsed += 1;
-                        None
-                    }
-                }
-            })
-            .filter_map(|(test, statements)| {
-                total_compiled += 1;
-                match compile::compile(statements, HashMap::new(), "Test".to_owned()) {
-                    Ok(class) => Some((test, class)),
-                    Err(error) => {
-                        eprintln!("failed to compile {}: {}", test.name, error);
-                        failed_compiled += 1;
-                        None
-                    }
-                }
-            });
-
-        for (test, class) in classes {
-            if run_test(&test, class) {
-                total_runs += 1;
-            } else {
-                failed_runs += 1;
-            }
-        }
-
-        println!(
-            "{}/{} failed to tokenize",
-            failed_tokenized, total_tokenized
+when greeting the user,
+    launch the app program
+",
+            "Hello, world!",
         );
-        println!("{}/{} failed to parse", failed_parsed, total_parsed);
-        println!("{}/{} failed to compile", failed_compiled, total_compiled);
-        println!("{}/{} failed to run", failed_runs, total_runs);
-
-        if failed_tokenized != 0 || failed_parsed != 0 || failed_compiled != 0 || failed_runs != 0 {
-            panic!("some tests failed");
-        }
-    }
-
-    #[test]
-    fn parser_features() {
-        run_tests(get_tests("parser/features"));
-    }
-
-    #[test]
-    fn runtime_features() {
-        run_tests(get_tests("parser/features"));
     }
 }
